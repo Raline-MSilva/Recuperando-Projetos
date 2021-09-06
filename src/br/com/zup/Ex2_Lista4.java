1dfdb83983280c62df209bffadc54ba4fe673694
Raline Silva <ralinesilva1@gmail.com>
2021-09-06 20:18:14 -0300

    resolvido

---
+++ src/br/com/zup/Ex2_Lista4.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class Ex2_Lista4 {
+    //2 - Faça um programa que mostra na tela os números de 100 a 1.
+
+    public static void main(String[] args) {
+        int contador = 100;
+        while (contador > 0){
+            System.out.println(contador);
+            contador --;
+        }
+    }
+}
